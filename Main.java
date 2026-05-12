bebd611deaa65b9eecfc782054f2d098022a1a31
suelias <eliaskafitz@gmail.com>
2026-05-12 09:18:36 +0200

    added var og

--- Main.java
+++ Main.java
@@ -4,13 +4,14 @@ public class Main
 {
     public static void main(String args[]){
 	int summe = 0;
-	for (int i = 1; i<= 20;i++)
+	int og = 10;
+	for (int i = 1; i<= og;i++)
 	{
             summe += i;
 	}        
 	
 	int mult = 0;
-	for(int i = 1; i<= 10;i++)
+	for(int i = 1; i<= og;i++)
 	{
 	    mult *= i;
         }
