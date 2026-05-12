9c2645826912cbddea070f54bae4698924911965
suelias <eliaskafitz@gmail.com>
2026-05-12 09:33:26 +0200

    coole idee 1

--- Main.java
+++ Main.java
@@ -1,23 +1,23 @@
 
-
 public class Main
 {
     public static void main(String args[]){
-	int summe = 0;
-	int obereGrenze = 10;
-	for (int i = 1; i<= obereGrenze;i++)
-	{
+        int summe = 0;
+        int obereGrenze = 10;
+        for (int i = 1; i<= obereGrenze;i++)
+        {
             summe += i;
-	}        
-	
-	int mult = 0;
-	for(int i = 1; i<= obereGrenze;i++)
-	{
-	    mult *= i;
-    }
+        }        
 
-	System.out.println("Summe = " + summe);
-	System.out.println("Mult = " + mult);
+        int mult = 0;
+        for(int i = 1; i<= obereGrenze;i++)
+        {
+            mult *= i;
+        }
 
+        System.out.println("Summe = " + summe);
+        System.out.println("Mult = " + mult);
+
+        //Coole Idee 1
     }
 }
